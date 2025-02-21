db16d063de222711a04d9d4713dd0843b05e4bba
raj625209 <raj625209@gmail.com>
2025-02-21 17:29:16 +0530

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }));
 
 // Constants for user details
 const USER_ID = "prince_ranjan_23112002";
